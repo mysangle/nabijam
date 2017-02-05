992c1d6e58c1f228bbcf84d4110159747e53f801
mysangle <mysangle@gmail.com>
2017-02-05 17:41:22 +0900

    add tab and view pager in MainActivity

--- app/src/main/java/com/twentyhours/nabijam/activity/MainActivity.kt
+++ app/src/main/java/com/twentyhours/nabijam/activity/MainActivity.kt
@@ -3,28 +3,37 @@ package com.twentyhours.nabijam.activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.Menu
-import android.view.MenuItem
 import com.twentyhours.nabijam.R
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import android.view.*
+import com.twentyhours.nabijam.fragment.ChatsFragment
+import com.twentyhours.nabijam.fragment.FriendsFragment
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    val toolbar = findViewById(R.id.toolbar) as Toolbar
-    setSupportActionBar(toolbar)
+
+    val mainViewPager = findViewById(R.id.main_view_pager) as ViewPager
+    val pagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+    pagerAdapter.addFragment(FriendsFragment(), "Friends")
+    pagerAdapter.addFragment(ChatsFragment(), "Chats")
+    mainViewPager.adapter = pagerAdapter
+
+    val mainTabs = findViewById(R.id.main_tabs) as TabLayout
+    mainTabs.setupWithViewPager(mainViewPager)
 
     val fab = findViewById(R.id.fab) as FloatingActionButton
     fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-          .setAction("Action", null).show()
+      showCreateAddressActivity()
     }
-
-    showCreateAddressActivity()
   }
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -48,5 +57,28 @@ class MainActivity : AppCompatActivity() {
 
   fun showCreateAddressActivity() {
     startActivity(Intent(this, CreateAddressActivity::class.java))
+    overridePendingTransition(R.anim.slide_in_right, R.anim.scale_down)
+  }
+
+  class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    private val mFragments = ArrayList<Fragment>()
+    private val mFragmentTitles = ArrayList<String>()
+
+    fun addFragment(fragment: Fragment, title: String) {
+      mFragments.add(fragment)
+      mFragmentTitles.add(title)
+    }
+
+    override fun getCount(): Int {
+      return mFragments.size
+    }
+
+    override fun getItem(position: Int): Fragment {
+      return mFragments[position]
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+      return mFragmentTitles[position]
+    }
   }
 }
---
+++ app/src/main/java/com/twentyhours/nabijam/fragment/FriendsFragment.kt
@@ -0,0 +1,24 @@
+package com.twentyhours.nabijam.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.twentyhours.nabijam.R
+
+/**
+ * Created by soonhyung on 2/5/17.
+ */
+class FriendsFragment : Fragment() {
+  override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    return inflater?.inflate(R.layout.content_main, container, false)
+  }
+
+  override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    val textView = view?.findViewById(R.id.title) as TextView
+    textView.text = "Friends"
+  }
+}
